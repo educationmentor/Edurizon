6dc6a5e145af8d4aac8f014ee0dc28151774621e
EdurizonWebsite <utkarshsaxena@rediffmail.com>
2025-03-22 01:53:41 +0530

    Remove height property from Ukraine flag image for improved responsiveness

--- frontend/pages/studyDestinations/study-mbbs-in-ukraine.tsx
+++ frontend/pages/studyDestinations/study-mbbs-in-ukraine.tsx
@@ -14,7 +14,7 @@ import Image from "next/image";
                         <Breadcrumbs/>
                     </div>
                     <div className="col-xl-2 col-lg-2 col-md-2 col-sm-3 col-3 text-end ml-auto">
-                        <Image alt="banglades flag" width={100} height={100} src="/assets/images/countryflag/ukraineflag2.png" className="img-fluid" />
+                        <Image alt="banglades flag" width={100} height={100} src="/assets/images/countryflag/ukraineflag2.png" />
                     </div>
                     </div>
                 </div>
