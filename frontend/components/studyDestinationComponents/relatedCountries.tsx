204db440c96f85456f371436a8f7a915302b0223
EdurizonWebsite <utkarshsaxena@rediffmail.com>
2025-03-21 17:17:37 +0530

    Remove unused study destinations page and update university links for better navigation; add new country flag images

---
+++ frontend/components/studyDestinationComponents/relatedCountries.tsx
@@ -0,0 +1,262 @@
+import Image from 'next/image';
+const RelatedCountries=()=>{
+    return(
+        <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+
+  <div className="heading">
+
+    <h3 className="mt-0 mb-4 mb-md-4">Related Countries</h3>
+
+  </div>
+
+  <div className="grid grid-cols-3 justify-content-center">
+
+      <div className="col-xl-4 col-lg-4 col-md-6 col-6 mb-4 mb-md-4 mx-[1vw]">
+
+      <div className="related-countries-detail">
+
+        <a href="https://edurizon.in/mbbs-in-india.php">
+
+          <div className="related-countries-detail-img">
+
+            <Image width={100} height={100} src="/assets/images/country-flag/indian-flag.png" className="img-fluid" alt="MBBS in India" title="MBBS in India" />
+
+          </div>
+
+          <h4>MBBS in India</h4>
+
+          <p>Read More</p>
+
+        </a>
+
+      </div>
+
+    </div>
+
+    <div className="col-xl-4 col-lg-4 col-md-6 col-6 mb-4 mb-md-4 mx-[1vw]">
+
+      <div className="related-countries-detail">
+
+        <a href="mbbs-in-russia.php">
+
+          <div className="related-countries-detail-img">
+
+            <Image width={100} height={100} src="/assets/images/country-flag/russia.png" className="img-fluid" alt="MBBS in Russia" title="MBBS in Russia" />
+
+          </div>
+
+          <h4>MBBS in Russia</h4>
+
+          <p>Read More</p>
+
+        </a>
+
+      </div>
+
+    </div>
+
+    <div className="col-xl-4 col-lg-4 col-md-6 col-6 mb-4 mb-md-4 mx-[1vw]">
+
+      <div className="related-countries-detail">
+
+        <a href="mbbs-in-bangladesh.php">
+
+          <div className="related-countries-detail-img">
+
+            <Image width={100} height={100}  src="/assets/images/country-flag/bangladesh.png" className="img-fluid" alt="MBBS in Bangladesh" title="MBBS in Bangladesh" />
+
+          </div>
+
+          <h4>MBBS in Bangladesh</h4>
+
+          <p>Read More</p>
+
+        </a>
+
+      </div>
+
+    </div>
+
+    <div className="col-xl-4 col-lg-4 col-md-6 col-6 mb-4 mb-md-4 mx-[1vw]">
+
+      <div className="related-countries-detail">
+
+        <a href="mbbs-in-china.php">
+
+          <div className="related-countries-detail-img">
+
+            <Image width={100} height={100}  src="/assets/images/country-flag/china.png" className="img-fluid" alt="MBBS in China" title="MBBS in China" />
+
+          </div>
+
+          <h4>MBBS in China</h4>
+
+          <p>Read More</p>
+
+        </a>
+
+      </div>
+
+    </div>
+
+    <div className="col-xl-4 col-lg-4 col-md-6 col-6 mb-4 mb-md-4 mx-[1vw]">
+
+      <div className="related-countries-detail">
+
+        <a href="mbbs-in-kazakhstan.php">
+
+          <div className="related-countries-detail-img">
+
+            <Image width={100} height={100}  src="/assets/images/country-flag/kazakhstan.png" className="img-fluid" alt="MBBS in Kazakhstan" title="MBBS in Kazakhstan" />
+
+          </div>
+
+          <h4>MBBS in Kazakhstan</h4>
+
+          <p>Read More</p>
+
+        </a>
+
+      </div>
+
+    </div>
+
+    <div className="col-xl-4 col-lg-4 col-md-6 col-6 mb-4 mb-md-4 mx-[1vw]">
+
+      <div className="related-countries-detail">
+
+        <a href="mbbs-in-georgia.php">
+
+          <div className="related-countries-detail-img">
+
+            <Image width={100} height={100}  src="/assets/images/country-flag/georgia.png" className="img-fluid" alt="MBBS in Georgia" title="MBBS in Georgia" />
+
+          </div>
+
+          <h4>MBBS in Georgia</h4>
+
+          <p>Read More</p>
+
+        </a>
+
+      </div>
+
+    </div>
+
+    <div className="col-xl-4 col-lg-4 col-md-6 col-6 mb-4 mb-md-4 mx-[1vw]">
+
+      <div className="related-countries-detail">
+
+        <a href="https://edurizon.in/mbbs-in-uzbekistan.php">
+
+          <div className="related-countries-detail-img">
+
+            <Image width={100} height={100}  src="/assets/images/country-flag/uzbekistan.png" className="img-fluid" alt="MBBS in Germany" title="MBBS in Germany" />
+
+          </div>
+
+          <h4>MBBS in Uzbekistan</h4>
+
+          <p>Read More</p>
+
+        </a>
+
+      </div>
+
+    </div>
+
+    <div className="col-xl-4 col-lg-4 col-md-6 col-6 mb-4 mb-md-4 mx-[1vw]">
+
+      <div className="related-countries-detail">
+
+        <a href="https://edurizon.in/study-in-germany.php">
+
+          <div className="related-countries-detail-img">
+
+            <Image width={100} height={100}  src="/assets/images/country-flag/german.png" className="img-fluid" alt="MBBS in Germany" title="MBBS in Germany" />
+
+          </div>
+
+          <h4>Study in Germany</h4>
+
+          <p>Read More</p>
+
+        </a>
+
+      </div>
+
+    </div>
+
+    <div className="col-xl-4 col-lg-4 col-md-6 col-6 mb-4 mb-md-4 mx-[1vw]">
+
+      <div className="related-countries-detail">
+
+        <a href="mbbs-in-nepal.php">
+
+          <div className="related-countries-detail-img">
+
+            <Image width={100} height={100}  src="/assets/images/country-flag/nepal.png" className="img-fluid" alt="MBBS in Nepal" title="MBBS in Nepal" />
+
+          </div>
+
+          <h4>MBBS in Nepal</h4>
+
+          <p>Read More</p>
+
+        </a>
+
+      </div>
+
+    </div>
+  </div>
+  <div  className="flex flex-row justify-center justify-content-center">
+  <div className="col-xl-4 col-lg-4 col-md-6 col-6 mb-4 mb-md-4 w-[380px] mx-[1vw]">
+
+<div className="related-countries-detail ">
+
+  <a href="mbbs-in-ukraine.php">
+
+    <div className="related-countries-detail-img">
+
+      <Image width={100} height={100}  src="/assets/images/country-flag/ukraine.png" className="img-fluid" alt="MBBS in Ukraine" title="MBBS in Ukraine" />
+
+    </div>
+
+    <h4>MBBS in Ukriane</h4>
+
+    <p>Read More</p>
+
+  </a>
+
+</div>
+
+</div>
+
+<div className="col-xl-4 col-lg-4 col-md-6 col-6 mb-4 mb-md-4 w-[380px] mx-[1vw]">
+
+<div className="related-countries-detail">
+
+  <a href="mbbs-in-egypt.php">
+
+    <div className="related-countries-detail-img">
+
+      <Image width={100} height={100}  src="/assets/images/country-flag/egypt-flag.png" className="img-fluid" alt="MBBS in Egypt" title="MBBS in Egypt" />
+
+    </div>
+
+    <h4>MBBS in Egypt</h4>
+
+    <p>Read More</p>
+
+  </a>
+
+</div>
+
+</div>
+  </div>
+
+</div>
+    )
+}
+
+export default RelatedCountries;
