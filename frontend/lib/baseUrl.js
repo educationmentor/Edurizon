e37a19f84448aaf7450bd2bf628c7f6152e8d394
noddy0070 <utkarshsaxena@rediffmail.com>
2026-01-31 03:31:12 +0530

    minor changes

--- frontend/lib/baseUrl.js
+++ frontend/lib/baseUrl.js
@@ -1,4 +1,4 @@
-export const baseUrl = process.env.NEXT_PUBLIC_API_URL || 'https://edurizon-glw4.vercel.app/';
+export const baseUrl = process.env.NEXT_PUBLIC_API_URL || 'https://edurizon-glw4.vercel.app';
 // export const baseUrl="http://localhost:5000" 
 
     
